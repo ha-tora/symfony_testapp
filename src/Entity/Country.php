3b0bf26177c76a4dc341b0f610b69fd668a36560
ha_tora <j.2374.04@gmail.com>
2023-03-10 15:23:51 +0600

    Entities implemetation

---
+++ src/Entity/Country.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountryRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
+class Country
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 2)]
+    private ?string $code = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $tin_pattern = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 4, scale: 2)]
+    private ?string $tax = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getTinPattern(): ?string
+    {
+        return $this->tin_pattern;
+    }
+
+    public function setTinPattern(string $tin_pattern): self
+    {
+        $this->tin_pattern = $tin_pattern;
+
+        return $this;
+    }
+
+    public function getTax(): ?string
+    {
+        return $this->tax;
+    }
+
+    public function setTax(string $tax): self
+    {
+        $this->tax = $tax;
+
+        return $this;
+    }
+}
